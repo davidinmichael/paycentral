f014d5b7c972eb18e91af71580f56e3e5998d1d6
davidinmichael <davidinmichael@gmail.com>
2023-10-19 16:02:37 +0100

    I ran the python manage.py collectstatic to save static files for production

--- paycentral/settings.py
+++ paycentral/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$c)0&**!vgf&o+!hxj976v8=z*#t@vkf1-0i-cbwq)ks0(+vzf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
