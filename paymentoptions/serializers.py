95e4e5c38182f30bab6a68ee2e66f7f6069a7fa3
davidinmichael <davidinmichael@gmail.com>
2023-10-20 23:19:27 +0100

    Add payment serializer modified

--- paymentoptions/serializers.py
+++ paymentoptions/serializers.py
@@ -15,15 +15,6 @@ class PaymentOptionCountrySerializer(serializers.ModelSerializer):
         fields = ['payment_option', 'supported_countries']
 
 
-class AddPaymentOptionSerializer(serializers.ModelSerializer):
-    payment_option = serializers.CharField(max_length=255)
-    country = CountrySerializer(many=True)
-
-    class Meta:
-        model = PaymentOption
-        fields = ['payment_option', 'country']
-
-
 
 class SingleCountrySerializer(serializers.ModelSerializer):
     region = serializers.StringRelatedField()
@@ -33,24 +24,25 @@ class SingleCountrySerializer(serializers.ModelSerializer):
         fields = ["name", "capital", "region", "payment_options"]
 
 
-# class AddPaymentOptionSerializer(serializers.ModelSerializer):
-#     payment_option = serializers.CharField(max_length=255)
-#     country = serializers.ListField(child=serializers.CharField(max_length=100))
+class AddPaymentOptionSerializer(serializers.ModelSerializer):
+    payment_option = serializers.CharField(max_length=255)
+    country = serializers.SlugRelatedField(queryset=Country.objects.all(),
+                                       slug_field="name", many=True)
 
-#     class Meta:
-#         model = PaymentOption
-#         fields = ['payment_option', 'country']
+    class Meta:
+        model = PaymentOption
+        fields = ['payment_option', 'country']
 
-#     def create(self, validated_data):
-#         supported_country_names = validated_data.pop('country', [])
+    def create(self, validated_data):
+        supported_country_names = validated_data.pop('country', [])
 
-#         payment_option = PaymentOption.objects.create(payment_option=validated_data['payment_option'])
+        payment_option = PaymentOption.objects.create(payment_option=validated_data['payment_option'])
 
-#         for country_name in supported_country_names:
-#             country = Country.objects.get(name=country_name)
-#             payment_option.country.add(country)
+        for country_name in supported_country_names:
+            country = Country.objects.get(name=country_name)
+            payment_option.country.add(country)
 
-#         return payment_option
+        return payment_option
 
 
 # {
