5d9577d71af616e02477b19844f234a5914e4bc0
davidinmichael <davidinmichael@gmail.com>
2023-10-20 19:19:31 +0100

    I reverted the add payment serializer

--- paymentoptions/serializers.py
+++ paymentoptions/serializers.py
@@ -15,13 +15,13 @@ class PaymentOptionCountrySerializer(serializers.ModelSerializer):
         fields = ['payment_option', 'supported_countries']
 
 
-class AddPaymentOptionSerializer(serializers.ModelSerializer):
-    payment_option = serializers.CharField(max_length=255)
-    country = serializers.SlugRelatedField(queryset=Country.objects.all(), slug_field="country")
+# class AddPaymentOptionSerializer(serializers.ModelSerializer):
+#     payment_option = serializers.CharField(max_length=255)
+#     country = serializers.SlugRelatedField(queryset=Country.objects.all(), slug_field="name")
 
-    class Meta:
-        model = PaymentOption
-        fields = ['payment_option', 'country']
+#     class Meta:
+#         model = PaymentOption
+#         fields = ['payment_option', 'country']
 
 
 class SingleCountrySerializer(serializers.ModelSerializer):
@@ -32,25 +32,24 @@ class SingleCountrySerializer(serializers.ModelSerializer):
         fields = ["name", "capital", "region", "payment_options"]
 
 
-# class AddPaymentOptionSerializer(serializers.ModelSerializer):
-#     payment_option = serializers.CharField(max_length=255)
-#     country = serializers.ListField(child=serializers.CharField(max_length=100))
+class AddPaymentOptionSerializer(serializers.ModelSerializer):
+    payment_option = serializers.CharField(max_length=255)
+    country = serializers.ListField(child=serializers.CharField(max_length=100))
 
-#     class Meta:
-#         model = PaymentOption
-#         fields = ['payment_option', 'country']
+    class Meta:
+        model = PaymentOption
+        fields = ['payment_option', 'country']
 
-#     def create(self, validated_data):
-#         supported_country_names = validated_data.pop('country', [])
-#         print(type(supported_country_names))
+    def create(self, validated_data):
+        supported_country_names = validated_data.pop('country', [])
 
-#         payment_option = PaymentOption.objects.create(payment_option=validated_data['payment_option'])
+        payment_option = PaymentOption.objects.create(payment_option=validated_data['payment_option'])
 
-#         for country_name in supported_country_names:
-#             country = Country.objects.get(name=country_name)
-#             payment_option.country.add(country)
+        for country_name in supported_country_names:
+            country = Country.objects.get(name=country_name)
+            payment_option.country.add(country)
 
-#         return payment_option
+        return payment_option
 
 
 # {
