7f61ab58af5b32a4116b4202a1aa011876f8fe81
davidinmichael <davidinmichael@gmail.com>
2023-11-15 09:43:38 +0100

    Template corrected

--- paycentral/settings.py
+++ paycentral/settings.py
@@ -147,5 +147,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "paycentral.payments@gmail.com"
-EMAIL_HOST_PASSWORD = "iabfczvkdpyilqas"
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
