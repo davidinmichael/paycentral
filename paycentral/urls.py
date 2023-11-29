aebd27af6a7e96bc01e12e90b3768d55a5d98827
davidinmichael <davidinmichael@gmail.com>
2023-11-29 09:38:34 +0100

    I added the media urls

--- paycentral/urls.py
+++ paycentral/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,4 +21,4 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("", include("paymentoptions.urls")),
     path("", include("countries.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
